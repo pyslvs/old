fe182cb1356422fbcb57710df5b7ef1e09fe227d
s40723145 <40723145@gm.nfu.edu.tw>
2020-12-01 21:40:43 +0800

    add the quick return mechanism example

--- init.py
+++ init.py
@@ -9,7 +9,7 @@ _curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
 config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
-    uwsgi = True 
+    uwsgi = False 
     site_title = "pyslvs.com"
     ip = "127.0.0.1"
     port = 9443
