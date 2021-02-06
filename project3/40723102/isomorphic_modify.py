cb27a8d282e8e566acd4a7634bbb306703e3f5f8
s40723145 <40723145@gm.nfu.edu.tw>
2021-02-06 12:00:17 +0800

    add the modified file for isomorphic

---
+++ project3/40723102/isomorphic_modify.py
@@ -0,0 +1,75 @@
+from pyslvs.graph import *
+import tkinter
+from tkinter import messagebox 
+ 
+def test():
+    #dof=3*(NL-1)-2*(J1)-J2
+    #nl=NL=Number of links.
+    #nj=J1=Number of joints. 
+    ##J2=Two degree of freedom
+
+    [dof,J2]=[1,0]
+    nl=6 #Input
+    nj=( dof-3*(nl-1)+J2 )/-2
+    type1= link_synthesis(nl , nj)
+    #print(type(type1))
+
+    cg_list =contracted_graph(type1[0]) 
+    c_j_list=contracted_link_synthesis(type1[0])
+
+    L=[]
+    for  x in range(3):
+        answer = conventional_graph(cg_list, c_j_list[x])
+        if answer == []:
+            continue
+        L.append(answer)
+    #print(L)
+    return L
+
+print(f"ALL: {test()}",
+    "-" * 80,
+    f"第2維: {test()[0][0]}",
+    sep="\n")
+
+###################### modify ##############################
+
+graph1 = test()[0][0]
+graph2 = test()[1][0]
+
+def isomorphic(graph1, graph2):
+    #Determine whether it is isomorphic
+    result = Graph.is_isomorphic(graph1, graph2)
+    
+    if result == True:
+        print("The graphs are isomorphic")
+    else:
+        # print("OK")
+        pass
+        
+isomorphic(graph1, graph2)
+
+###################### modify ##############################
+
+"""
+def triangle():
+    #Determine  whether Graph has triangle
+    g1=test()[0][0]
+    g2=test()[1][0]
+    result1= Graph.has_triangle(g1)
+    result2= Graph.has_triangle(g2)
+    if result1:
+        root = tkinter .Tk()
+        root.withdraw()#隱藏視窗
+        tkinter.messagebox.showerror('Tiangle Error','g1 hatriangle.')
+    elif result2:
+        root = tkinter .Tk()
+        root.withdraw()#隱藏視窗
+        tkinter.messagebox.showerror('Triangle Error','g2 has triangle.')
+    else:
+        root = tkinter .Tk()
+        root.withdraw()#隱藏視窗
+        tkinter.messagebox.showinfo('Triangle warning','g1&g2 do not have triangles.')
+print(triangle())
+"""
+
+
