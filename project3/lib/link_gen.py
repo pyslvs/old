1c01c80fe01cc4172bd717032f631cba3f3754d1
s40723145 <40723145@gm.nfu.edu.tw>
2021-02-25 15:30:34 +0800

    modify the part dimension parameters

---
+++ project3/lib/link_gen.py
@@ -0,0 +1,82 @@
+import pythoncom
+import win32com.client
+import win32api
+import os
+
+
+class inv:
+    def __init__(self):
+        """     Initialize the settings of the Inventor     """
+        self.invApp = win32com.client.Dispatch("Inventor.Application")
+        self.invApp.Visible = True
+        self.invApp.SilentOperation = True
+
+    def open(self, partName: str):
+        """     Open Part     """
+        try:
+            self.oDoc = self.invApp.Documents.Open(partName)
+        except:
+            print("The part opened error !!")
+        
+    def parameter(self, **kwargs):
+        """     Modify the dimension of the part     """
+        for key, value in kwargs.items():
+            # print(key, value)
+            try:
+                par = self.oDoc.ComponentDefinition.Parameters.Item(str(key))
+            # Convert the unit from "cm" to "mm"
+                par.Value = value * (10**-1)
+            except:
+                print("The parameter doesn't include in part.")
+                break
+            
+        is_update = self.oDoc.Update()
+        if is_update == None:
+            print("The part is updated.")
+        else:
+            print("Fail !")
+
+    def save_as(self, newfilename: str):
+        """     Save as the new part     """
+        self.Doc.SaveAs(newfilename)
+        
+    def close(self):
+        """     Close Inventor     """
+        self.invApp.Quit()
+
+
+def link_gen_import_test():
+    filename = os.path.basename(__file__)
+    return filename + " import successfully !"
+
+
+if __name__ == "__main__":
+    inv = inv()
+    
+    """
+    inv.open('Y:/pyslvs.io/project3/40723145/binary_link.ipt')
+    inv.parameter(
+        # binary link
+        x1=40, y1=55,
+        x2=80, y2=85,
+        hole=3,
+        thickness=3
+    )
+    """
+    
+    
+    """
+    inv.open('Y:/pyslvs.io/project3/40723145/ternary_link.ipt')
+    inv.parameter(
+        # ternary link
+        x1=140, y1=138,
+        x2=210, y2=225,
+        x3=234, y3=125,
+        hole=10,
+        thickness=3
+    )
+    """
+    
+
+
+
--- project3/main.py
+++ project3/main.py
@@ -5,56 +5,64 @@ import re
 # print(synthesis_import_test())
 # print(link_gen_import_test())
 
-num_links = 4
-pos = {
-    0: (0, 0),
-    1: (90, 0),
-    2: (12.92, 32.53),
-    3: (73.28, 67.97),
-    4: (33.3, 66.95)
-}
-input = [(0, 2), (0, 360)]
-placement = {0: (-70, -70, 10), 1: (70, -70, 10)}
-pass_point = {
-    4: [
-        (60.3, 118.12),
-        (31.02, 115.62),
-        (3.52, 110.62),
-        (-25.77, 104.91),
-        (-81.49, 69.19),
-        (-96.47, 54.906),
-        (-109.34, 35.98),
-        (-121.84, 13.83),
-        (-127.56, -20.09),
-        (-128.63, -49.74),
-        (-117.56, -65.45),
-    ]
-}
-# pass three points: {[(60.3, 118.12), (31.02, 115.62), (3.52, 110.62)]}
-cus = {4: 2}
 
-set = settings(num_links, pos, input, placement, pass_point, cus)
-mechanism = set.collection()
-result = algorithm_RGA(mechanism)
-print(result)
-# print(f"The expression of synthesis result:\n{result}")
+def synthesis(set: settings) -> str:
+    mechanism = set.collection()
+    mech_expr = algorithm_RGA(mechanism)
+    # print(f"The expression of synthesis result:\n{mech_expr}")
+    return mech_expr
 
 
+def get_joint_coord(mech_expr: str) -> list([(float, float)]):
+    coord_list = []
+    XorY_coord_list = re.findall("[-]*\d*[.]\d*", mech_expr)
+    for index in range(len(XorY_coord_list)):
+        if index % 2 != 0:
+            continue
+        else:
+            coord = (float(XorY_coord_list[index]), float(XorY_coord_list[index + 1]))
+            coord_list.append(coord)
+    # print(coord_list)
+    return coord_list
 
-# organize mechanism expression of the joint coordinate and let it be the type of tuple in the list that we can call it later.
-coord_list = []
-XorY_coord_list = re.findall("[-]*\d*[.]\d*", result)
-for index in range(len(XorY_coord_list)):
-    if index % 2 != 0:
-        continue
-    else:
-        coord = (float(XorY_coord_list[index]), float(XorY_coord_list[index + 1]))
-        coord_list.append(coord)
+    
+if __name__ == "__main__":
+### define the parameters ###
+    num_links = 4
+    pos = {
+        0: (0, 0),
+        1: (90, 0),
+        2: (12.92, 32.53),
+        3: (73.28, 67.97),
+        4: (33.3, 66.95)
+    }
+    input = [(0, 2), (0, 360)]
+    placement = {0: (-70, -70, 10), 1: (70, -70, 10)}
+    pass_point = {
+        4: [
+            (60.3, 118.12),
+            (31.02, 115.62),
+            (3.52, 110.62),
+            (-25.77, 104.91),
+            (-81.49, 69.19),
+            (-96.47, 54.906),
+            (-109.34, 35.98),
+            (-121.84, 13.83),
+            (-127.56, -20.09),
+            (-128.63, -49.74),
+            (-117.56, -65.45),
+        ]
+    }
+    cus = {4: 2}
 
-print(coord_list)
-print(coord_list[4][1])
-print(type(coord_list[4][1]))
-
+    set = settings(num_links, pos, input, placement, pass_point, cus)
+    # print(type(set))
+    ### define the parameters ###
+    
+    mech_expr = synthesis(set)
+    print(mech_expr)
+    joint_coord = get_joint_coord(mech_expr)
+    print(joint_coord)
 
 
 
