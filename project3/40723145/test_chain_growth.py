3fff44d1d4ff60f31ce0d6438516185cd540af84
s40723145 <40723145@gm.nfu.edu.tw>
2021-03-05 22:37:09 +0800

    add the test chain_growth file

---
+++ project3/40723145/test_chain_growth.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+def distance(x1, y1, x2, y2):
+    result = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return result
+    
+
+def get_chain_growth(Lo, Lg):
+    # Lo: origin length
+    # Lg: growth length
+    return (Lg - Lo) / Lo
+    
+    
+if __name__ == "__main__":
+    # The lowest position chain growth:
+    # left_pivot(-15, -34), right_pivot(-2, -21)
+
+    # The highest position chain growth:
+    # left_pivot(-31, 3.5), right_pivot(-3, -15.5)
+
+    Lo = distance(-15, -34, -2, -21)
+    Lg = distance(-31, 3.5, -3, -15.5)
+    print("origin length: ", Lo)
+    print("growth length: ", Lg)
+    
+    chain_growth = get_chain_growth(Lo, Lg)
+    print("chain_growth: ", chain_growth)
